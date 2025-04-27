4ef756eb7cc71e6104f691a5a7d531de8ec9f0b9
What521 <rubenkgeorge@gmail.com>
2025-04-27 13:35:12 -0400

    finalServer

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 const Joi = require("joi");
+const mongoose = require("mongoose");
 
 
 const storage = multer.diskStorage({
@@ -23,113 +24,33 @@ app.get("/",(req, res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
-let movies = [
-  {
-    be_id: "1",
-    title: "John Wick",
-    Genre: "Action",
-    image1: "images/JWimg.jfif",
-    image2: "images/JW2.jfif",
-    desc: "Legendary assassin John Wick (Keanu Reeves) retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) -- John's former colleague -- puts a huge bounty on John's head.",
-    cast: "Keanu Reeves, Adrianne Palicki",
-    year: "2014",
-    rating: "7.4/10 IMDb"
-},
-{
-    be_id: "2",
-    title: "The Fall Guy",
-    Genre: "Action",
-    image1: "images/FGimg.jfif",
-    image2: "images/FG2.jfif",
-    desc: "After leaving the business one year earlier, battle-scarred stuntman Colt Seavers springs back into action when the star of a big studio movie suddenly disappears. As the mystery surrounding the missing actor deepens, Colt soon finds himself ensnared in a sinister plot that pushes him to the edge of a fall more dangerous than any stunt.",
-    cast: "Ryan Gosling, Emily Blunt",
-    year: "2024",
-    rating: "6.8/10 IMDb"
-},
-{
-    be_id: "3",
-    title: "Flight Risk",
-    Genre: "Action",
-    image1: "images/FR.jfif",
-    image2: "images/fr2.jfif",
-    desc: "A U.S. marshal boards a small plane to transfer a government witness to New York. As they cross the Alaskan wilderness, tensions start to rise as not everyone on the flight is who they appear to be.",
-    cast: "Mark Wahlberg, Michelle Dockery",
-    year: "2025",
-    rating: "5.3/10 IMDb"
-},
-{
-    be_id: "4",
-    title: "Free Guy",
-    Genre: "Comedy",
-    image1: "images/FreeG.jfif",
-    image2: "images/freeG2.jfif",
-    desc: "When a bank teller discovers he's actually a background player in an open-world video game, he decides to become the hero of his own story -- one that he can rewrite himself. In a world where there's no limits, he's determined to save the day his way before it's too late, and maybe find a little romance with the coder who conceived him.",
-    cast: "Ryan Reynolds, Jodie Comer",
-    year: "2021",
-    rating: "7.1/10 IMDb"
-},
-{
-    be_id: "5",
-    title: "White Chicks",
-    Genre: "Comedy",
-    image1: "images/WC.jfif",
-    image2: "images/wc2.jfif",
-    desc: "Two FBI agent brothers, Marcus (Marlon Wayans) and Kevin Copeland (Shawn Wayans), accidentally foil a drug bust. As punishment, they are forced to escort a pair of socialites (Anne Dudek, Rochelle Aytes) to the Hamptons, where they're going to be used as bait for a kidnapper. But when the girls realize the FBI's plan, they refuse to go. Left without options, Marcus and Kevin decide to pose as the sisters, transforming themselves from African-American men into a pair of blonde, white women.",
-    cast: "Shawn Wayans, Marlon Wayans",
-    year: "2004",
-    rating: "5.8/10 IMDb"
-},
-{
-    be_id: "6",
-    title: "Fly me to the moon",
-    Genre: "Comedy",
-    image1: "images/flmm.jfif",
-    image2: "images/flmm2.jfif",
-    desc: "A marketing specialist, Kelly Jones, is tasked with staging a fake moon landing as a backup plan in case the real Apollo 11 mission fails, leading to a romantic comedy-drama set against the backdrop of the space race.",
-    cast: "Channing Tatum, Scarlett Johanessen",
-    year: "2024",
-    rating: "6.6/10 IMDb"
-},
-{
-    be_id: "7",
-    title: "La La Land",
-    Genre: "Romance",
-    image1: "images/LLL.jfif",
-    image2: "images/LLL2.jfif",
-    desc: "Sebastian (Ryan Gosling) and Mia (Emma Stone) are drawn together by their common desire to do what they love. But as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.",
-    cast: "Ryan Gosling, Emma Stone",
-    year: "2016",
-    rating: "8/10 IMDb"
-},
-{
-    be_id: "8",
-    title: "The Idea of You",
-    Genre: "Romance",
-    image1: "images/idea.jfif",
-    image2: "images/idea2.jfif",
-    desc: "A 40-year-old single mum begins an unexpected romance with a 24-year-old boy band singer.",
-    cast: "Anne Hathaway, Nicholas Galitzine",
-    year: "2024",
-    rating: "6.3/10 IMDb"
-},
-{
-    be_id: "9",
-    title: "Anyone But You",
-    Genre: "Romance",
-    image1: "images/anyone.jfif",
-    image2: "images/anyone2.jfif",
-    desc: "Despite having an amazing first date, Bea and Ben's initial attraction quickly turns sour. When they unexpectedly find themselves at a destination wedding in Australia, the pair pretend to be the perfect couple to keep up appearances.",
-    cast: "Sydney Sweeney, Glen Powell",
-    year: "2023",
-    rating: "6.1/10 IMDb"
-}
-];
+mongoose
+  .connect("mongodb+srv://rg50:George521!@cluster0.qq9s0li.mongodb.net/")
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("couldn't connect to mongodb", error);
+  });
 
-app.get("/api/movies", (req, res)=>{
+  const movieSchema = new mongoose.Schema({
+    title:String,
+    image1:String,
+    cast:String,
+    year:Number,
+    rating:String,
+  });
+
+
+  const Movie = mongoose.model("Movie", movieSchema);
+
+app.get("/api/movies", async(req, res)=>{
+    const movies = await Movie.find();
+    console.log(movies);
     res.send(movies);
 })
 
-app.post("/api/movies", upload.single("img"), (req,res)=>{
+app.post("/api/movies",  upload.single("img"), async(req,res)=>{
     const result = validateMovie(req.body);
 
     if(result.error){
@@ -138,30 +59,22 @@ app.post("/api/movies", upload.single("img"), (req,res)=>{
         return;
     }
 
-    const movie = {
-        be_id: movies.length,
+    const movie = new Movie({
         title:req.body.title,
-        Genre: req.body.Genre,
         cast: req.body.cast,
         year: req.body.year,
         rating: req.body.rating
-    };
+    });
 
     if(req.file){
-        movie.image1 = req.file.filename;
-    }
+      movie.image1 = req.file.filename;
+  }
 
-    movies.push(movie);
-    res.status(200).send(movie);
+    const newMovie = await movie.save();
+    res.status(200).send(newMovie);
 });
 
-app.put("/api/movies/:id", upload.single("img"),(req,res)=>{
-    const movie = movies.find((movie)=>movie.be_id===parseInt(req.params.id));
-
-    if(!movie){
-        res.status(404).send("The movie with the provided id was not found");
-        return;
-    }
+app.put("/api/movies/:id" , upload.single("img"), async(req,res)=>{
 
     const result = validateMovie(req.body);
 
@@ -170,40 +83,32 @@ app.put("/api/movies/:id", upload.single("img"),(req,res)=>{
         return;
     }
 
-    movie.title =req.body.title,
-    movie.Genre=req.body.Genre,
-    movie.cast=req.body.cast,
-    movie.year = req.body.year,
-    movie.rating = req.body.rating
+    const fieldsToUpdate = {
+        title:req.body.title,
+        cast:req.body.cast,
+        year:req.body.year,
+        rating:req.body.rating
+    }
+
+    const wentThrough = await Movie.updateOne({_id:req.params.id},fieldsToUpdate);
+    const movie = await Movie.findOne({_id:req.params.id});
 
     if(req.file){
-        movie.main_image = req.file.filename;
-    }
+      movie.main_image = req.file.filename;
+  }
 
     res.status(200).send(movie);
 });
 
-app.delete("/api/movies/:id",(req,res)=>{
-    console.log("I'm trying to delete" + req.params.id);
-    const movie = movies.find((movie)=>movie.be_id===parseInt(req.params.id));
-
-    if(!movie){
-        console.log("Oh no i wasn't found");
-        res.status(404).send("The movie with the provided id was not found");
-        return;
-    }
-    console.log("YAY You found me");
-    console.log("The movie you are deleting is " + movie.name);
-    const index = movies.indexOf(movie);
-    movies.splice(index,1);
+app.delete("/api/movies/:id" ,async(req,res)=>{
+    const movie = await Movie.findByIdAndDelete(req.params.id);
     res.status(200).send(movie);
-});
+}); 
 
 const validateMovie = (movie) => {
     const schema = Joi.object({
-        be_id:Joi.allow(""),
-        title:Joi.string().min(3).required(),
-        genre:Joi.string().required(),
+        _id:Joi.allow(""),
+        title:Joi.string().required(),
         cast:Joi.string().required(),
         year:Joi.number().required().min(1000),
         rating:Joi.string().required(),
